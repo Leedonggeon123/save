c16126aabcac675b6211552f6cecf26907ff5433
dong_geon <ekcnxs@gmail.com>
2026-09-12 19:44:16 +0900

    Fast api 테스트용 파일 업로드

---
+++ Fastapi_server/main.py
@@ -0,0 +1,17 @@
+# 서버 시작 api 연결
+
+
+
+
+
+from fastapi import FastAPI
+
+from .file_api import router                            # .으로 상대경로 사용
+
+
+app = FastAPI(                                          # fastapi 서버 객체 생성
+    title="JewelCloud Server",
+)
+
+
+app.include_router(router)                                  # 파일 관련 api를 전체 서버에 등록
