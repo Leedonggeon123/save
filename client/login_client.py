f073b9b625f7071dc4ae0b932d80dafbd953434c
dong_geon <ekcnxs@gmail.com>
2026-09-13 19:55:13 +0900

    프로젝트 기본적인 틀만 잡음

---
+++ client/login_client.py
@@ -0,0 +1,17 @@
+from socket_client import connect_socket
+
+
+
+# ===========================
+# 클라이언트 (로그인/회원가입)
+# ===========================
+
+
+user_id = 1                                                             # 테스트 user_id
+
+connect_socket(user_id)                                                  # 소켓 호출
+
+
+
+
+
