0ac668e3d2b0115cdefdf6572800802772d18e38
yj3677 <dudwn3677@gmail.com>
2026-09-15 14:25:52 +0900

    로그인/회원가입 리팩토링 작업 중

--- client/ui_common.py
+++ client/ui_common.py
@@ -12,7 +12,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-SERVER_URL = os.getenv("JEWEL_SERVER_URL", "http://127.0.0.1:8000").rstrip("/")
+SERVER_URL = os.getenv("JEWEL_SERVER_URL", "http://10.10.10.107:8000").rstrip("/")
 
 
 def logo_pixmap(path, width=72, height=72):
